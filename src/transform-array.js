f5c8e5fdaea74461a61fb8a51ee124b63fe2662c
Marina Kistanova <48287166+k4noise@users.noreply.github.com>
2021-01-15 18:51:25 +0500

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let result = [];
+  const len = arr.length;
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  for (let i = 0; i < len; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        if (i < len - 3) {
+          if (arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev')
+            i++;
+        }
+        i++;
+        break;
+      case '--discard-prev':
+        if (result.length) {
+          result.pop();
+        }
+        break;
+      case '--double-next':
+        if (i != len - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i) {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+  return result;
 };
